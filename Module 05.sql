44b8b370145727ad4921bdb3bff91f2e1e3f830e
Dmitrii Terlikov <Dmitrii_Terlikov@epam.com>
2019-10-16 09:19:45 +0400

    M5 corrections.

--- Module 05.sql
+++ Module 05.sql
@@ -1,6 +1,6 @@
 USE [Northwind]
 GO
-/****** Object:  StoredProcedure [dbo].[CustOrderHist]    Script Date: 15.10.2019 21:58:53 ******/
+/****** Object:  StoredProcedure [dbo].[GreatestOrders]  Script Date: 15.10.2019 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -12,14 +12,13 @@ SET ROWCOUNT @top
 
 SELECT Concat(e.LastName, ' ', e.Firstname) as Employee, ag.EmployeeID, ag.[Max Fright]
 FROM Employees e 
-INNER JOIN
-(SELECT o.EmployeeID, Max(Cast((o.Freight - (o.Freight * od.Discount)) AS decimal(10,2))) as [Max Fright]
-FROM Orders o
-INNER JOIN Employees e ON e.EmployeeID = o.EmployeeID
-INNER JOIN [Order Details] OD ON OD.OrderId = o.OrderId
-WHERE Year(o.OrderDate) = @year
-GROUP BY o.EmployeeID
-) ag ON e.EmployeeID = ag.EmployeeID
+INNER JOIN (SELECT o.EmployeeID, Max(Cast((o.Freight - (o.Freight * od.Discount)) AS decimal(10,2))) as [Max Fright]
+			FROM Orders o
+			INNER JOIN Employees e ON e.EmployeeID = o.EmployeeID
+			INNER JOIN [Order Details] od ON od.OrderId = o.OrderId
+			WHERE Year(o.OrderDate) = @year
+			GROUP BY o.EmployeeID
+			) ag ON e.EmployeeID = ag.EmployeeID
 ORDER BY [Max Fright] DESC
 
 SET ROWCOUNT 0
