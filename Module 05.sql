97e30826dabe06431bba1dd23c7f4d007f5e0410
Dmitrii Terlikov <Dmitrii_Terlikov@epam.com>
2019-10-23 09:07:31 +0400

    upper case.

--- Module 05.sql
+++ Module 05.sql
@@ -10,12 +10,12 @@ CREATE PROCEDURE [dbo].[GreatestOrders] @year INT, @top INT
 AS
 
 SELECT TOP (@top) CONCAT(e.LastName, ' ', e.Firstname) as Employee, ag.OrderID, CAST(ag.[Order Price] AS DECIMAL(10,2)) AS [Order Price]
-FROM Employees e
-Inner Join Orders o ON e.EmployeeID = o.EmployeeID
-Inner Join (SELECT o.OrderID, o.EmployeeID, SUM(od.[Quantity] * (od.[UnitPrice] - od.[UnitPrice] * od.[Discount] )) AS [Order Price]
+FROM dbo.Employees e
+INNER JOIN Orders o ON e.EmployeeID = o.EmployeeID
+INNER JOIN (SELECT o.OrderID, o.EmployeeID, SUM(od.[Quantity] * (od.[UnitPrice] - od.[UnitPrice] * od.[Discount] )) AS [Order Price]
 			, ROW_NUMBER() OVER (PARTITION BY o.EmployeeId ORDER BY SUM(od.[Quantity] * (od.[UnitPrice] - od.[UnitPrice] * od.[Discount] )) DESC) AS N
-			FROM Orders o
-			inner Join [Order Details] od ON o.OrderID = od.OrderID
+			FROM dbo.Orders o
+			INNER JOIN dbo.[Order Details] od ON o.OrderID = od.OrderID
 			WHERE  YEAR(o.OrderDate) = @year
 			GROUP BY o.OrderID, o.EmployeeID) ag ON o.OrderID = ag.OrderID
 WHERE ag.N = 1
@@ -25,37 +25,37 @@ GO
 -------------------------------------------------
 --[dbo].[GreatestOrdersCur]
 -------------------------------------------------
-IF OBJECT_ID('[dbo].[GreatestOrdersCur]', 'P') IS NOT NULL Drop Procedure [dbo].[GreatestOrdersCur]
+IF OBJECT_ID('[dbo].[GreatestOrdersCur]', 'P') IS NOT NULL DROP PROCEDURE [dbo].[GreatestOrdersCur]
 GO
 CREATE PROCEDURE [dbo].[GreatestOrdersCur] @year int, @top int
 AS
 
-Declare @employee nvarchar(50)
-Declare @id int
-Declare @mFright decimal(10,2)
-Declare cur CURSOR
-For
+DECLARE @employee nvarchar(50)
+DECLARE @id int
+DECLARE @mFright decimal(10,2)
+DECLARE cur CURSOR
+FOR
 
 SELECT CONCAT(e.LastName, ' ', e.Firstname) as Employee, ag.OrderID, CAST(ag.[Order Price] AS DECIMAL(10,2)) AS [Order Price]
 FROM Employees e
-Inner Join Orders o ON e.EmployeeID = o.EmployeeID
-Inner Join (SELECT o.OrderID, o.EmployeeID, SUM(od.[Quantity] * (od.[UnitPrice] - od.[UnitPrice] * od.[Discount] )) AS [Order Price]
+INNER JOIN Orders o ON e.EmployeeID = o.EmployeeID
+INNER JOIN (SELECT o.OrderID, o.EmployeeID, SUM(od.[Quantity] * (od.[UnitPrice] - od.[UnitPrice] * od.[Discount] )) AS [Order Price]
 			, ROW_NUMBER() OVER (PARTITION BY o.EmployeeId ORDER BY SUM(od.[Quantity] * (od.[UnitPrice] - od.[UnitPrice] * od.[Discount] )) DESC) AS N
 			FROM Orders o
-			inner Join [Order Details] od ON o.OrderID = od.OrderID
+			INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
 			WHERE  YEAR(o.OrderDate) = @year
 			GROUP BY o.OrderID, o.EmployeeID) ag ON o.OrderID = ag.OrderID
 WHERE ag.N = 1
 ORDER BY [Order Price] DESC
 
-Open cur
+OPEN cur
 
-While @top > 0
-Begin
-	Fetch Next From cur Into @employee, @id, @mFright
-	Select @employee as Employee, @id as [Order], @mFright as [Order Price]
+WHILE @top > 0
+BEGIN
+	FETCH NEXT FROM cur Into @employee, @id, @mFright
+	SELECT @employee as Employee, @id as [Order], @mFright as [Order Price]
 	Set @top = @top - 1
-End
+END
 
 Close cur
 Deallocate cur
@@ -65,51 +65,51 @@ GO
 -------------------------------------------------
 --[dbo].[ShippedOrdersDiff]
 -------------------------------------------------
-IF OBJECT_ID('[dbo].[ShippedOrdersDiff]', 'P') IS NOT NULL Drop Procedure [dbo].[ShippedOrdersDiff]
+IF OBJECT_ID('[dbo].[ShippedOrdersDiff]', 'P') IS NOT NULL DROP PROCEDURE [dbo].[ShippedOrdersDiff]
 GO
 CREATE PROCEDURE [dbo].[ShippedOrdersDiff] @days int
 AS
 
-If (IsNull(@days, '') = '')
+IF (ISNULL(@days, '') = '')
 	Set @days = 35
 
-Select  OrderID, OrderDate, ShippedDate, Datediff(Day, OrderDate, ShippedDate) as ShippedDelay, @days as SpecifiedDelay 
-From Orders
-Where ShippedDate - OrderDate > @days Or ShippedDate is Null
+SELECT  OrderID, OrderDate, ShippedDate, Datediff(Day, OrderDate, ShippedDate) as ShippedDelay, @days as SpecifiedDelay 
+FROM Orders
+WHERE ShippedDate - OrderDate > @days Or ShippedDate is Null
 GO
 
 
 --------------------
 --IsBoss
 --------------------
-IF OBJECT_ID('[dbo].[IsBoss]', 'FN') IS NOT NULL Drop Function [dbo].[IsBoss]
+IF OBJECT_ID('[dbo].[IsBoss]', 'FN') IS NOT NULL DROP FUNCTION [dbo].[IsBoss]
 GO
-Create Function dbo.IsBoss(@id int)
-Returns Bit
+CREATE Function dbo.IsBoss(@id int)
+RETURNS BIT
 AS
-Begin
-	IF Exists(Select top 1 null From Employees Where ReportsTo = @id)
-	Begin
-		Return 1
-	End
-	Return 0
-End
+BEGIN
+	IF Exists(SELECT top 1 null FROM Employees WHERE ReportsTo = @id)
+	BEGIN
+		RETURN 1
+	END
+	RETURN 0
+END
 GO
 
 
 --------------------
 --Real Prices View 
 --------------------
-IF OBJECT_ID('[dbo].[RealPrices]', 'V') IS NOT NULL Drop View [dbo].[RealPrices]
+IF OBJECT_ID('[dbo].[RealPrices]', 'V') IS NOT NULL DROP VIEW [dbo].[RealPrices]
 GO
-Create View RealPrices As
-Select o.OrderID, o.CustomerID, Concat(e.LastName, ' ', e.Firstname) as Employee, o.OrderDate, o.RequiredDate
+CREATE VIEW RealPrices AS
+SELECT o.OrderID, o.CustomerID, Concat(e.LastName, ' ', e.Firstname) as Employee, o.OrderDate, o.RequiredDate
 	 , p.ProductName, Cast(od.UnitPrice - od.UnitPrice * od.Discount as decimal(10,2)) as Price
-From Orders o
-Inner Join Employees e On e.EmployeeID = o.EmployeeID
-Inner Join [Order Details] od On o.OrderID = od.OrderID
-Inner Join Products p On p.ProductID = od.ProductID
-Order By Price OffSet 0 Rows
+FROM Orders o
+INNER JOIN Employees e ON e.EmployeeID = o.EmployeeID
+INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
+INNER JOIN Products p ON p.ProductID = od.ProductID
+ORDER BY Price OffSet 0 ROWS
 GO
 
 ----------------------------------------------------------------------------------------------------------------
@@ -118,9 +118,9 @@ GO
 -- Данные поля выбраны для того чтобы можно было однозначно идентифицировать объект и субъект манипуляции данными,
 -- а так же для опеределения даты и времени на которую нужно выполнить откат измененных данных.
 ----------------------------------------------------------------------------------------------------------------
-IF OBJECT_ID('[dbo].[_OrdersHistory]', 'U') IS NOT NULL Drop Table [dbo].[_OrdersHistory]
+IF OBJECT_ID('[dbo].[_OrdersHistory]', 'U') IS NOT NULL DROP TABLE [dbo].[_OrdersHistory]
 GO
-Create Table dbo._OrdersHistory
+CREATE Table dbo._OrdersHistory
 (
 	Id int Identity(1,1),
 	ActionType nvarchar(50) Not Null,
@@ -130,33 +130,33 @@ Create Table dbo._OrdersHistory
 )
 GO
 
-IF OBJECT_ID('[dbo].[HisTrgr]', 'TR') IS NOT NULL Drop Trigger [dbo].[HisTrgr]
+IF OBJECT_ID('[dbo].[HisTrgr]', 'TR') IS NOT NULL DROP TRIGGER [dbo].[HisTrgr]
 GO
-Create Trigger HisTrgr
-On [dbo].[Orders]
+CREATE Trigger HisTrgr
+ON [dbo].[Orders]
 After Insert, Update, Delete
-As
-Begin
-	declare @rowId nvarchar(50)
-	declare @aType char(6)
+AS
+BEGIN
+	DECLARE @rowId nvarchar(50)
+	DECLARE @aType char(6)
 
-	If exists (Select 1 From inserted) AND not exists (Select 1 From deleted)
-	Begin
+	IF exists (SELECT 1 FROM inserted) AND not exists (SELECT 1 FROM deleted)
+	BEGIN
 		Set @atype = 'Insert'
-		Set @rowid = (Select ins.OrderID From inserted ins)
-	End Else
-	Begin
-		if exists (Select 1 From deleted) and not exists (Select 1 From inserted)
-		Begin
+		Set @rowid = (SELECT ins.OrderID FROM inserted ins)
+	END Else
+	BEGIN
+		if exists (SELECT 1 FROM deleted) and not exists (SELECT 1 FROM inserted)
+		BEGIN
 			Set @atype = 'Delete'
-			Set @rowid = (Select d.OrderID From deleted d)
-		End Else
-		Begin
+			Set @rowid = (SELECT d.OrderID FROM deleted d)
+		END Else
+		BEGIN
 			Set @atype = 'Update'
-		    Set @rowid = (Select ins.OrderID From inserted ins)
-		End
-	End
+		    Set @rowid = (SELECT ins.OrderID FROM inserted ins)
+		END
+	END
 
-	Insert Into dbo._OrdersHistory(ActionType, ActionDate, [User], RowId)
-	Values(@aType, GETDATE(), SYSTEM_USER, @rowId)
-End
+	INSERT INTO dbo._OrdersHistory(ActionType, ActionDate, [User], RowId)
+	VALUES(@aType, GETDATE(), SYSTEM_USER, @rowId)
+END
