ff79c05da91033c81213edb235a6e5e8b91d7d82
Dmitry <diman3003@mail.ru>
2019-11-04 12:51:31 +0400

    add m7.

--- QueryM7.sql
+++ QueryM7.sql
@@ -7,7 +7,7 @@ GO
 -------------------------------------------------------------------------------------------------------------------------------------
 ;WITH CTE
 AS(
-	SELECT cat.CategoryName, c.Region AS R, CAST((od.Quantity * (od.UnitPrice - od.UnitPrice * od.Discount )) AS decimal(10,2)) as P
+	SELECT cat.CategoryName, c.Region AS R, CAST((od.Quantity * (od.UnitPrice - od.UnitPrice * od.Discount )) AS decimal(10,2)) AS P
 	FROM dbo.Customers c 
 	INNER JOIN dbo.Orders o ON c.CustomerID = o.CustomerID
 	INNER JOIN dbo.[Order Details] od ON o.OrderID = od.OrderId
@@ -17,13 +17,13 @@ AS(
 )
 SELECT CategoryName as Category, [AK], [BC], [CA], [Co. Cork]
 FROM CTE
-PIVOT(AVG(P) FOR R IN ([AK], [BC], [CA], [Co. Cork])) as PT;
+PIVOT(AVG(P) FOR R IN ([AK], [BC], [CA], [Co. Cork])) AS PT;
 GO
-exec sp_help #Periods
+
 -----------------------------------------------------------------------------
 -- 7.2.	—оздать временную таблицу #Periods с двум€ пол€ми: PeriodID, Value. 
 -----------------------------------------------------------------------------
-IF OBJECT_ID('[dbo].[#Periods]', 'U') IS NOT NULL DROP TABLE [dbo].[#Periods];
+IF OBJECT_ID('tempdb..#Periods') IS NOT NULL DROP TABLE [dbo].[#Periods];
 GO
 
 CREATE TABLE dbo.#Periods
@@ -49,7 +49,7 @@ INSERT INTO #Periods([Period], [Value]) VALUES(10,40);
 ;WITH CTE
 AS
 (
-	SELECT [Period], [Value], SUM(Value) OVER(ORDER BY [Period] ROWS BETWEEN  1 PRECEDING AND CURRENT ROW) AS V
+	SELECT [Period], [Value], SUM([Value]) OVER(ORDER BY [Period] ROWS BETWEEN  1 PRECEDING AND CURRENT ROW) AS V
 	FROM #Periods
 )
 SELECT [Period]
@@ -64,7 +64,7 @@ GO
 ;WITH CTE
 AS
 (
-	SELECT [Period], [Value], SUM(Value) OVER(ORDER BY [Period] ROWS BETWEEN  1 PRECEDING AND CURRENT ROW) AS V
+	SELECT [Period], [Value], SUM([Value]) OVER(ORDER BY [Period] ROWS BETWEEN  1 PRECEDING AND CURRENT ROW) AS V
 	FROM #Periods
 )
 DELETE
@@ -78,24 +78,58 @@ SELECT * FROM #Periods
 -- 7.3.1 ѕронумеруйте заказы из таблицы Orders в пор€дке уменьшени€ времени затрат на доставку.
 -------------------------------------------------------------------------------------------------
 
-SELECT OrderID, OrderDate, ShippedDate, DATEDIFF(day, OrderDate, ShippedDate) as Delta
-		,ROW_NUMBER() OVER(ORDER BY DATEDIFF(day, OrderDate, ShippedDate) DESC) as N
+SELECT OrderID, OrderDate, ShippedDate, DATEDIFF(day, OrderDate, ShippedDate) AS Delta
+		,ROW_NUMBER() OVER(ORDER BY DATEDIFF(day, OrderDate, ShippedDate) DESC) AS N
 FROM dbo.Orders
 WHERE ShippedDate IS NOT NULL
 
 -------------------------------------------------------------------------------------------------------
 -- 7.3.2 ¬ыберите те категории продуктов, у которых количество поставщиков из одной страны больше трех.
 -------------------------------------------------------------------------------------------------------
-;With CTE
+;WITH CTE
 AS
 (
-	SELECT s.SupplierID, ProductID, s.Country, c.CategoryID, c.CategoryName
-			, ROW_NUMBER() OVER(Partition BY c.CategoryID, Country ORDER BY c.CategoryID, Country ) as N
-	FROM dbo.Suppliers s
-	INNER JOIN dbo.Products p ON p.SupplierID = s.SupplierID
-	INNER JOIN dbo.Categories c ON c.CategoryID = p.CategoryID
+	SELECT *, ROW_NUMBER() OVER(PARTITION BY CategoryName, Country ORDER BY Country ) AS N
+	FROM
+	(
+		SELECT DISTINCT s.SupplierID, s.Country, c.CategoryName
+		FROM dbo.Suppliers s
+		INNER JOIN dbo.Products p ON p.SupplierID = s.SupplierID
+		INNER JOIN dbo.Categories c ON c.CategoryID = p.CategoryID
+	) AS t
 )
 
 SELECT CategoryName
-from CTE
-WHERE N = 4 
+FROM CTE
+WHERE N > 1
+GO
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- 7.3.3.	—оздайте функцию GetNums(@low bigint, @high bigint), котора€ возвращает таблицу с колонкой n bigint.
+-- ¬ этой таблице должны быть упор€доченные по возрастанию значени€ от @low до @high (количеством записей @high - @low + 1).
+-----------------------------------------------------------------------------------------------------------------------------
+IF OBJECT_ID(N'[dbo].[GetNums]', N'FN') IS NOT NULL DROP FUNCTION [dbo].[GetNums]
+GO
+
+CREATE FUNCTION dbo.GetNums(@low bigint, @high bigint)
+RETURNS @t TABLE   
+(  
+    n bigint  
+)  
+AS  
+BEGIN  
+WITH Cte(n)
+    AS (  
+			SELECT @low [n]
+			UNION ALL
+			SELECT [n]+1 FROM Cte WHERE [n] < @high
+        )  
+
+   INSERT @t
+   SELECT n
+   FROM Cte  
+   RETURN  
+END;  
+GO 
+
+SELECT * FROM dbo.GetNums(1,5)
